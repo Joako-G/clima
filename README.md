11499fda73a7a6c566e5e379746743579d3515b8
Joako-G <gallardojoako@gmail.com>
2022-08-23 18:06:20 -0300

    Initial commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# web-platform-ngl5pq
+
+[Edit on StackBlitz ⚡️](https://stackblitz.com/edit/web-platform-ngl5pq)
---
+++ script.js
@@ -0,0 +1,27 @@
+const URL =
+  'https://api.openweathermap.org/data/2.5/weather?lat=-24.18&lon=-65.33&lang=es&units=metric&appid=ec72693024ff3528187442d9519966be';
+
+const list = document.querySelector('.cities');
+fetch(URL)
+  .then((response) => response.json())
+  .then((data) => {
+    const { main, name, sys, weather } = data;
+    const icon = `https://s3-us-west-2.amazonaws.com/s.cdpn.io/162656/${weather[0]['icon']}.svg`;
+    const temp = main.temp.toFixed(1);
+    const div = document.createElement('div');
+
+    console.log(data);
+    div.classList.add('city');
+    const markup = ` <h2 class="city-name" data-name="${name},${sys.country}">
+    <span>${name}</span>
+    <sup>${sys.country}</sup>
+    </h2>
+    <div class="city-temp">${temp}<sup>°C</sup></div>
+    <figure>
+    <img class="city-icon" src=${icon} alt="${weather[0]['description']}">
+    <figcaption>${weather[0]['description']}</figcaption>
+    </figure>`;
+    div.innerHTML = markup;
+    list.appendChild(div);
+  });
+//-24.18330509829115, -65.33130861869898
